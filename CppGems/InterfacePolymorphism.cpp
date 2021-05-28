d9345145a1d410df4acec0f08314922a3ba0d37e
Harish A Kulkarni <harishakulkarni@gmail.com>
2021-05-28 10:28:22 +0530

    Interface, calling of function in multi level inheritance
    When an Interface is given and implementation is in library in classes spread across multiple level of inheritance, you create the object using the class that you have implemented in your code deriving from most derived class. But if the implementation is not there in mentioned class, compiler searches the down to top and first available implementation of the function is called.

---
+++ CppGems/InterfacePolymorphism.cpp
@@ -0,0 +1,44 @@
+// CppGems.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Base 
+{
+ 
+public:
+    virtual void prepare() 
+    {
+        std::cout << "Class: Base" << std::endl;
+    }
+};
+
+class Derived1 :public Base
+{
+
+public:
+    virtual void prepare()
+    {
+        std::cout << "class: Derived1" << std::endl;
+    }
+};
+
+class Derived2 : public Derived1
+{
+public:
+    
+};
+
+class Derived3 : public Derived2
+{
+    virtual void prepare()
+    {
+        return Derived2::prepare();
+    }
+};
+
+int main()
+{
+    Base* p = new Derived3;
+    p->prepare();
+}
