d561a15e44cb678cfc4331617b84dca229b38582
Harish A Kulkarni <harishakulkarni@gmail.com>
2021-08-25 13:58:25 +0530

    Access in struct vs class
    Difference between class and struct in C++

--- CppGems/Source.cpp
+++ CppGems/Source.cpp
@@ -1,7 +1,15 @@
-#include "ImplementationDefined.h"
+#include <iostream>
 
+#include "ImplementationDefined.h"
+#include "StructvsClass.h"
+void testDefaultMemberAccess() 
+{
+    Intro introToBook;
+    Index indexOfBook;
+    std::cout << introToBook.length; //throws compilation error since members are private by default in class
+    std::cout << indexOfBook.totalTopics; //accessible outside struct because members are public by default
+}
 int main()
 {
-    ImplementationDefined implDefBehaviour;
-    implDefBehaviour.TestImplementationDefinedBehaviour();
+    testDefaultMemberAccess();
 }
---
+++ CppGems/StructvsClass.h
@@ -0,0 +1,10 @@
+class Intro 
+{
+	int length = 0;
+	double pageSize = 10.1;
+
+};
+struct Index
+{
+	int totalTopics = 10;
+};
