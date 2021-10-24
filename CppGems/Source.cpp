dcc79effe2f3c30d1e8e8eba2e49f743f5a02122
Harish A Kulkarni <harishakulkarni@gmail.com>
2021-10-24 17:44:09 +0530

    Problem solving Day 2
    Right rotate of matrix. - Transpose and column swap - O(n2)

--- CppGems/Source.cpp
+++ CppGems/Source.cpp
@@ -176,6 +176,30 @@ int primalPowerOfArray(std::vector<int>& A)
     }
     return count;
 }
+void swap(int& A, int& B) 
+{
+    int temp = A;
+    A = B;
+    B = A;
+}
+void swampWithoutTemp(int& A, int& B) 
+{
+    A = A + B; // A has both A and B
+    B = A - B; // B has A now
+    A = A - B; // A has B now
+}
+
+void transposeMatrix(std::vector <std::vector<int>> &A) 
+{
+    int N = A.size();
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < i; j++) 
+        {
+            swampWithoutTemp(A[i][j], A[j][i]);
+        }
+    }
+}
 
 void testDefaultMemberAccess() 
 {
@@ -383,40 +407,37 @@ int oddEvenSubStringCount(std::vector<int> &A)
     return count;
 }
 
+void columnSwap(std::vector<std::vector<int>> &A) 
+{
+    int N = A.size();
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < N/2; j++) 
+        {
+            swampWithoutTemp(A[i][j], A[i][N - j - 1]);
+        }
+    }
+}
+
+void rotate(std::vector < std::vector<int>>& A) 
+{
+    transposeMatrix(A);
+    columnSwap(A);
+}
 int main()
 {
-    //std::cout << addBinary("10111111","00111111");
-    //std::cout << addBinary("10001100010111000101100010100110001001101010000010011010", "101111000100100100111110010010101110101001100100101001111010011000000110");
-    //numbersAndBitManipulation();
-    /*std::cout << maxValue(3,5) << std::endl;
-    std::cout << maxValue(3, 3) << std::endl;
-    std::cout << maxValue(-1, 13) << std::endl;
-    std::cout << maxValue(-1, -13) << std::endl;
-    std::cout << maxValue(-1, -1.5) << std::endl;
-    std::cout << maxValue('a', 'b') << std::endl;
-    std::cout << maxValue('A', 'Z') << std::endl;
-    std::cout << maxValue('a', 'A') << std::endl;*/
-    /*int A[] = { 0, 1, 2, 3, 4, 5};
-    subArray(A,sizeof(A)/sizeof(A[0]));*/
-    /*std::vector<std::string> a = fizzBuzz(31);
-    for (int i = 0; i < a.size(); i++)
-        std::cout << a[i] << std::endl;
-    return 0;
-    std::vector<int> A = { 97, 43, 29, 11, 100, 47, 76, 83, 37, 19, 17, 19, 71, 0, 1, -82, 2, -83, 37, 13, 5, 97, 17, 30, 31, 48, 2, 19, 31, 91, 19, 2, 5, 89, 2, 67, 31, 47, 43, 31, 5, 17, 83, 11, 47, 73, 19, 3, 3, 19, 59, 91, 67, 7, 43, 4, 3, 51, 52, 23, 3, 37, 53, 89, 9, 41, 19, 61, 7, 5, 53, 59, 19, 11, 79, 37, 31, 37, 73, 82, 41, 2, 13, 8, 2, 36, 19, 58, 17, 17, 59, 59, 37, 11, 13, 37, 47, 83, 31, 3 };
-    std::cout << primalPowerOfArray(A);*/
-    //std::vector <int> A = { 0, 3, 7, 6, 4, 0, 5, 5, 5 };
-    //std::vector<int> output = plusOne(A);
-    //for (int i = 0; i < output.size(); i++)
-    //    std::cout << output[i] << ' ';
-    /*std::vector<int> A = { 1,2,3,4,5 };
-    std::vector<int> B = { 2,3 };
-    multipleLeftRotate(A, B);*/
-    std::vector<std::vector<int>> output = genereateSquareSpiralMatrix(5);
-    for (int i = 0; i < output.size(); i++) 
+    
+    std::vector<std::vector<int>> A = { 
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    rotate(A);
+    for (int i = 0; i < A.size(); i++) 
     {
-        for (int j = 0; j < output[i].size(); j++) 
+        for (int j = 0; j < A[i].size(); j++) 
         {
-            std::cout << output[i][j] << '\t';
+            std::cout << A[i][j] << '\t';
         }
         std::cout << std::endl;
     }
